81ced8a294eed5f679b318f18d6b091b5194bab3
Septimas <septimas25@gmail.com>
2023-05-18 23:15:16 +0300

    number multiplied/divided

---
+++ Home_work_3/task_2.py
@@ -0,0 +1,17 @@
+number = 2
+# multiplying by 2
+result = number * 2
+print(result)
+
+number *= 2
+print(number)
+
+# Divide in 2
+result = number / 2
+print(result)
+
+number /= 2
+print(number)
+
+# When running the "divide" code - it shows incorrect results if "multiply" parts are not commented out. So comment
+# out a "multiply" parts of code before running the "divide" code.
