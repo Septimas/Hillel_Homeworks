182bcb8fa2b52c32b9721a78621e9d2cabdbd593
Septimas <septimas25@gmail.com>
2023-05-18 20:27:42 +0300

    fixed comment

--- task_6.py
+++ task_6.py
@@ -1,5 +1,3 @@
 names = ['Dean', 'Ally', 'Dean', 'Jane']
-print(names.index('Dean'))
-names.pop(0)
-names2 = names.copy()
-print(names2)
+unique_names=list(dict.fromkeys(names))
+print(unique_names)
