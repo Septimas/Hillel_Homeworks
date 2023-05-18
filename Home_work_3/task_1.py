d0578eaeebc2383ab7870b33a508183dde0c4afb
Septimas <septimas25@gmail.com>
2023-05-18 23:13:21 +0300

    values ordered

--- Home_work_3/task_1.py
+++ Home_work_3/task_1.py
@@ -1,2 +1,15 @@
 elements = [1, 2, 3, 4, 5, 6, 7, 8]
+odd = []
+even = []
 
+# for number in elements:
+#     print(number)
+
+for index, number in enumerate(elements):
+    if index % 2 == 0:
+        even.append((index, number))
+    else:
+        odd.append((index, number))
+
+print(f'Odd index: {odd}')
+print(f'Even index: {even}')
