25c63674e6e22753b9f030d43b2cf2bb99437905
Septimas <septimas25@gmail.com>
2023-05-20 14:15:45 +0300

    james not in console

--- Home_work_3/task_3.py
+++ Home_work_3/task_3.py
@@ -2,7 +2,9 @@ friends = ['John', 'Marta', 'James']
 enemies = ['John', 'Johnatan', 'Artur']
 
 for friend in friends:
-    if friend in enemies:
+    if friend == 'James':
+        continue
+    elif friend in enemies:
         print(f'{friend}, we are not friends anymore')
     else:
         print(f'{friend}, we are the best friends')
